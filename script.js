7da4af79da3d8d4cf820cb1aa8581799b5dd4e5b
Damian Mirek <damianmirek1234@gmail.com>
2025-02-16 19:38:45 +0100

    Files add

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const faqClickHandler = function (event) {
+  event.stopPropagation();
+
+  const { target } = event;
+  const clickedFAQElement = target.closest(".faq-element");
+  const hiddenContentElement = clickedFAQElement.querySelector(
+    ".faq-hidden-content"
+  );
+  const isExpanding = hiddenContentElement.style.height === "";
+
+  clickedFAQElement.classList.toggle("faq-content-expanded");
+
+  if (isExpanding) {
+    hiddenContentElement.style.height = `${hiddenContentElement.scrollHeight}px`;
+    hiddenContentElement.style.margin = "0 9.6rem 3.2rem";
+  } else {
+    hiddenContentElement.style.height = "";
+    hiddenContentElement.style.margin = "0 9.6rem";
+  }
+};
+
+const faqSection = document.querySelector("section.faq");
+faqSection.addEventListener("click", faqClickHandler);
+
+const currentYear = new Date().getFullYear();
+const footerRightsSpan = document.querySelector(".footer-rights");
+footerRightsSpan.textContent = `© Copyright ${currentYear} by Petpal LLC. All rights reserved`;
+
+const mainNavList = document.querySelector(".main-header-nav-list ");
+const mainNavListClickHandler = (event) => {
+  const { target } = event;
+  const hrefAttribute = target.getAttribute("href");
+  const scrollDestination = document.querySelector(hrefAttribute);
+  scrollDestination.scrollIntoView({ behavior: "smooth", block: "start" });
+};
+
+mainNavList.addEventListener("click", mainNavListClickHandler);
